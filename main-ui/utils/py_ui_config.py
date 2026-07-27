7b89e1877e0bb066b3b2d14a73c868596230d7ad
chrisj951 <chrisj951@gmail.com>
2026-07-27 19:43:12 -0400

    Add updates by Felixmil for smart pro s home button
    Also remove some video playback settings that were accidentally merged
    without needed CFW changes

--- main-ui/utils/py_ui_config.py
+++ main-ui/utils/py_ui_config.py
@@ -203,10 +203,6 @@ class PyUiConfig:
         return cls.get("activityLogPath",None)
 
     @classmethod
-    def get_video_root_path(cls):
-        return cls.get("videoRootPath", "/mnt/SDCARD/Roms/MEDIA")
-
-    @classmethod
     def get_activity_log_path(cls):
         return cls.get("activityLogPath",None)
     
